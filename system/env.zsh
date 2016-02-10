8e7987fb336ec3d7b41a826eaa76e7d7c563ba43
Marc-Aurèle Brothier <m@brothier.org>
2016-02-10 14:14:28 +0100

    system: change LC_* to en_US

--- system/env.zsh
+++ system/env.zsh
@@ -1,6 +1,6 @@
-export EDITOR='subl'
+export EDITOR='vim'
 
 # Fix LC on OSX
-export LANG='en_CA.UTF-8'
-export LC_CTYPE='en_CA.UTF-8'
-export LC_ALL='en_CA.UTF-8'
+export LANG='en_US.UTF-8'
+export LC_CTYPE='en_US.UTF-8'
+export LC_ALL='en_US.UTF-8'
