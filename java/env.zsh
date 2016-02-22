ec9a92093a1e6139c2084ac4f1d9da7effe5c829
Marc-Aurèle Brothier <m@brothier.org>
2016-02-22 13:57:25 +0100

    java: split option as maven fork commands collide with debug port already open

--- java/env.zsh
+++ java/env.zsh
@@ -1,3 +1,4 @@
 eval "$(jenv init -)"
 
-export MAVEN_OPTS='-Xmx1024m -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n -Djava.net.preferIPv4Stack=true'
+export MAVEN_OPTS='-Xmx1024m -Djava.net.preferIPv4Stack=true'
+export MAVEN_OPTS_DEBUG='-Xmx1024m -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n -Djava.net.preferIPv4Stack=true'
