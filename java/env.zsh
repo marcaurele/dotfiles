4d821f627c4bb8fcc6e177ed6c0ba2f9cc9b4839
Marc-Aurèle Brothier <m@brothier.org>
2016-02-25 15:12:59 +0100

    java: split normal & debug

--- java/env.zsh
+++ java/env.zsh
@@ -1,4 +1,5 @@
 eval "$(jenv init -)"
 
-export MAVEN_OPTS='-Xmx1024m -Djava.net.preferIPv4Stack=true'
+export MAVEN_OPTS_DEFAULT='-Xmx1024m -Djava.net.preferIPv4Stack=true'
 export MAVEN_OPTS_DEBUG='-Xmx1024m -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n -Djava.net.preferIPv4Stack=true'
+export MAVEN_OPTS=$MAVEN_OPTS_DEFAULT
