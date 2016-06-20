d856dc38f2187940a6458c4209af0f8909456aaf
Marc-Aurèle Brothier <m@brothier.org>
2016-06-20 08:28:46 +0200

    exoscale:add demo alias to copy keys

--- exoscale/aliases.zsh
+++ exoscale/aliases.zsh
@@ -1,3 +1,6 @@
 alias cleanvmhosts='sed -i "" -E "/^159\.100/d" $HOME/.ssh/known_hosts'
 alias devdockerdb='grep -rIl -E "mysql.*127\.0\.0\.1" * | xargs sed -i "" -E "s/127.0.0.1/192.168.55.100/g"'
 alias gitpushmarco='git push origin -f HEAD:user/marco'
+
+alias demo_api='echo $DEMO_API_KEY | pbcopy'
+alias demo_secret='echo $DEMO_SECRET_KEY | pbcopy'
