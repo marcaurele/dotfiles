f16e6fcd204cba4a6da42abe3fc6af377dfd6213
Marc-Aurèle Brothier <m@brothier.org>
2017-02-08 10:18:03 +0100

    exo: edit docker db script alias

--- exoscale/aliases.zsh
+++ exoscale/aliases.zsh
@@ -1,5 +1,5 @@
 alias cleanvmhosts='sed -i "" -E "/^159\.100/d" $HOME/.ssh/known_hosts'
-alias devdockerdb='grep -rIl -E "mysql.*127\.0\.0\.1" envdir tests | xargs sed -i "" -E "s/127.0.0.1/192.168.55.100/g"'
+alias devdockerdb='grep -rIl -E "mysql.*127\.0\.0\.1" envdir tests | xargs sed -i "" -E "s/127.0.0.1/192.168.99.100/g"'
 alias gitpushmarco='git push origin -f HEAD:user/marco'
 
 alias demo_api='echo $DEMO_API_KEY | pbcopy'
