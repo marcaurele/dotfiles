44d5afd08520190cd6978eb986c281b74df58009
Marc-Aurèle Brothier <m@brothier.org>
2017-03-22 14:01:01 +0100

    Fix ZSH terminal colors and add completion colors

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -4,7 +4,8 @@ else
   export PS1='%3~$(git_info_for_prompt)%# '
 fi
 
-export LSCOLORS="exfxcxdxbxegedabagacad"
+export LSCOLORS="GxFxcxdxBxegedabagacad"
+export LS_COLORS="di=1;36:ln=1;35:so=32:pi=33:ex=1;31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 export CLICOLOR=true
 
 fpath=($ZSH/functions $fpath)
