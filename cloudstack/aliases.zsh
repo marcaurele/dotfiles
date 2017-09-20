2cb17206a3ea8c40f7c545380546a237353812a2
Marc-Aurèle Brothier <m@brothier.org>
2017-09-20 16:42:08 +0200

    cs: fix startup script

--- cloudstack/aliases.zsh
+++ cloudstack/aliases.zsh
@@ -3,5 +3,5 @@ alias csdev-build='mvn -P kvm,exoscale,developer -Dsimulator -Dmaven.test.skip=t
 alias csdev-buildwithtest='mvn -P kvm,exoscale,systemvm,developer -Dsimulator $1'
 alias csdev-deploydb='MAVEN_OPTS=$MAVEN_OPTS_DEBUG mvn -Pdeveloper -pl developer -Ddeploydb'
 alias csdev-deploysimulator='MAVEN_OPTS=$MAVEN_OPTS_DEBUG mvn -Pdeveloper -pl developer -Ddeploydb-simulator'
-alias csdev-start='java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=log4j-cloud.xml -Djetty.port=8080 -cp "client/target/conf:client/target/bcprov-jdk15on-1.55.jar:client/target/cloud-client-ui-4.4.2.9-exoscale-SNAPSHOT.jar" org.apache.cloudstack.ServerDaemon'
+alias csdev-start='java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=log4j-cloud.xml -Djetty.port=8080 -cp "client/target/conf:client/target/bcprov-jdk15on-1.55.jar:`find client/target -type f -name "cloud-client-*-exoscale-*"`" org.apache.cloudstack.ServerDaemon'
 alias csdev-installdc='python tools/marvin/marvin/deployDataCenter.py -i setup/dev/advanced.cfg'
