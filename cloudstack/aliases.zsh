7086dd5f59280bc4fbd1d1f9768382e909e9e197
Marc-Aurèle Brothier <m@brothier.org>
2017-09-23 12:59:38 +0200

    mysql: add dump with no lock option

--- cloudstack/aliases.zsh
+++ cloudstack/aliases.zsh
@@ -5,3 +5,5 @@ alias csdev-deploydb='MAVEN_OPTS=$MAVEN_OPTS_DEBUG mvn -Pdeveloper -pl developer
 alias csdev-deploysimulator='MAVEN_OPTS=$MAVEN_OPTS_DEBUG mvn -Pdeveloper -pl developer -Ddeploydb-simulator'
 alias csdev-start='java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=log4j-cloud.xml -Djetty.port=8080 -cp "client/target/conf:client/target/bcprov-jdk15on-1.55.jar:`find client/target -type f -name "cloud-client-*-exoscale-*"`" org.apache.cloudstack.ServerDaemon'
 alias csdev-installdc='python tools/marvin/marvin/deployDataCenter.py -i setup/dev/advanced.cfg'
+
+alias csdev-dump='mysqldump --complete-insert --skip-lock-tables --extended-insert=FALSE --ignore-table=cloud.usage_event --ignore-table=cloud.event'
