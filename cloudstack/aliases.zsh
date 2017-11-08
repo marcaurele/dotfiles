d42b59abfeb72cdea10c47358d3c1527d546495d
Marc-Aurèle Brothier <m@brothier.org>
2017-11-08 10:01:32 +0100

    cs: add JMX option for start

--- cloudstack/aliases.zsh
+++ cloudstack/aliases.zsh
@@ -3,7 +3,7 @@ alias csdev-build='mvn -P kvm,exoscale,developer -Dsimulator -Dmaven.test.skip=t
 alias csdev-buildwithtest='mvn -P kvm,exoscale,systemvm,developer -Dsimulator $1'
 alias csdev-deploydb='MAVEN_OPTS=$MAVEN_OPTS_DEBUG mvn -Pdeveloper -pl developer -Ddeploydb'
 alias csdev-deploysimulator='MAVEN_OPTS=$MAVEN_OPTS_DEBUG mvn -Pdeveloper -pl developer -Ddeploydb-simulator'
-alias csdev-start='java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=log4j-cloud.xml -cp "client/target/conf:client/target/bcprov-jdk15on-1.55.jar:`find client/target -type f -name "cloud-client-*-exoscale-*"`" org.apache.cloudstack.ServerDaemon'
+alias csdev-start='java -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=45219 -Dcom.sun.management.jmxremote.rmi.port=45219 -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=log4j-cloud.xml -cp "client/target/conf:client/target/bcprov-jdk15on-1.55.jar:`find client/target -type f -name "cloud-client-*-exoscale-*"`" org.apache.cloudstack.ServerDaemon'
 alias csdev-installdc='python tools/marvin/marvin/deployDataCenter.py -i setup/dev/advanced.cfg'
 
 alias csdev-dump='mysqldump --complete-insert --skip-lock-tables --extended-insert=FALSE --ignore-table=cloud.usage_event --ignore-table=cloud.event'
