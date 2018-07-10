54079dfe607753fdd24eb5ca9203cacc000da416
Marc-Aurèle Brothier <m@brothier.org>
2018-07-10 13:48:54 +0200

    gist: add zsh completion for python-gist

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,5 +1,6 @@
 # enable zsh-completions package
-fpath=(/usr/local/share/zsh-completions $fpath)
+#fpath=(/usr/local/share/zsh-completions $fpath)
+fpath=(${HOME}/.zsh $fpath)
 
 # matches case insensitive for lowercase
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
