f01f53188f52a2a0c886c67ffa8e2e05ed89438b
Marc-Aurèle Brothier <m@brothier.org>
2018-08-27 09:05:27 +0200

    osx: fix ssh-agent startup

--- osx/ssh-agent.zsh
+++ osx/ssh-agent.zsh
@@ -1,8 +1,5 @@
 # Little trick to start the SSH agent on OSX
 if [[ "$(uname -s)" == "Darwin" ]]; then
-    if [[ `ps -ef | grep -q ssh-agent$` ]]; then
-        echo "Starting new SSH agent..."
-        /usr/bin/ssh-agent -a /Users/marco/.ssh-agent.sock
-    fi
+    pgrep ssh-agent > /dev/null || /usr/bin/ssh-agent -a /Users/marco/.ssh-agent.sock
     export SSH_AUTH_SOCK="/Users/marco/.ssh-agent.sock"
 fi
